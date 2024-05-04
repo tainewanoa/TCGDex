45e149eb8690cf4d095fe768230ae11a7022a5c7
Taine Wanoa <wanoataine@gmail.com>
2024-05-04 13:08:28 +1200

    Index update

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Listening on port ${port}`);
+});
